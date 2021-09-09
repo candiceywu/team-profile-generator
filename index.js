fdbf222eac9d5da43f3e912bae943ef1da904382
candiceywu <candice.ying.wu@gmail.com>
2021-09-08 17:46:43 -0700

    html file

--- index.js
+++ index.js
@@ -1,13 +1,61 @@
+//include everything needed for this application
 const inquirer = require("inquirer");
-const Employee = require ("./lib/Employee");
-const Engineer = require ("./lib/Engineer");
-const Manager = require ("./lib/Manager");
-const Intern = require ("./lib/Intern");
+const fs = require("fs");
+const Employee = require("./lib/Employee");
+const Engineer = require("./lib/Engineer");
+const Manager = require("./lib/Manager");
+const Intern = require("./lib/Intern");
+const generateHTML = require("./dist/generateHTML");
 
 
 
-//
-// const bob = new Employee("bob", 11, "bob@mail.com")
-// const tina = new Engineer("tina", 1, "tina@mail.com", "awesometina")
-// console.log(bob.getRole())
-// console.log(tina.getGithub())
+// created an array of questions for user input
+function init() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: 'Please enter the team manager\'s name.',
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: 'Please enter the team manager\'s employee ID.',
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'Please enter the team manager\'s email address.',
+            },
+            {
+                type: 'input',
+                name: 'office',
+                message: 'Please enter the team manager\'s office number.',
+            },
+            {
+                type: 'rawlist',
+                name: 'staff',
+                message: 'Would you like to add an engineer or intern to your team roster?',
+                choices: ['Yes: engineer', 'Yes: intern', 'No, the roster is complete'],
+                // when: (answer) => answer.enterEngineer === true,
+            },
+
+        ])
+
+        // .then((answer) => {
+        //     if (answer.enterEngineer) {
+
+        //     }
+        // })
+
+        // function to initialize app
+        .then((data) => fs.writeFileSync('index.html', generateHTML(data)))
+        .then(() => console.log('Successfully created an HTML file.'))
+        .catch((err) => console.error(err));
+
+}
+
+// Function call to initialize app
+init();
+
