d9034d06aca5452443836aa5a9148d69bb9810c7
candiceywu <candice.ying.wu@gmail.com>
2021-09-08 17:01:39 -0700

    finished lib js files

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 const inquirer = require("inquirer");
+const Employee = require ("./lib/Employee");
+const Engineer = require ("./lib/Engineer");
+const Manager = require ("./lib/Manager");
+const Intern = require ("./lib/Intern");
+
+
+
+//
+// const bob = new Employee("bob", 11, "bob@mail.com")
+// const tina = new Engineer("tina", 1, "tina@mail.com", "awesometina")
+// console.log(bob.getRole())
+// console.log(tina.getGithub())
