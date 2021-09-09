85673fc528a3b4c83a1560e198740ef12d0aeeaa
candiceywu <candice.ying.wu@gmail.com>
2021-09-08 19:08:15 -0700

    reformatting inquirer with manager first

--- index.js
+++ index.js
@@ -5,54 +5,71 @@ const Employee = require("./lib/Employee");
 const Engineer = require("./lib/Engineer");
 const Manager = require("./lib/Manager");
 const Intern = require("./lib/Intern");
+const generateHTML = require("./src/generateHTML");
 
-
-const employees = [];
+//team array
+const teamArray = [];
 
 function init() {
     generateHtml();
     addTeamMember();
 }
 
-function addEmployee() {
+function addManager() {
     inquirer
         .prompt([
             {
                 type: 'input',
                 name: 'name',
-                message: 'Please enter the team member\'s name.',
+                message: 'Please enter the team manager\'s name:',
             },
             {
                 type: 'input',
                 name: 'id',
-                message: 'Please enter the team manager\'s employee ID.',
+                message: 'Please enter the manager\'s employee ID:',
             },
             {
                 type: 'input',
                 name: 'email',
-                message: 'Please enter the team manager\'s email address.',
+                message: 'Please enter the manager\'s email address:',
             },
             {
                 type: 'input',
-                name: 'office',
-                message: 'Please enter the team manager\'s office number.',
+                name: 'officeNumber',
+                message: 'Please enter the manager\'s office number:',
             },
             {
                 type: 'rawlist',
-                name: 'staff',
-                message: 'Would you like to add an engineer or intern to your team roster?',
-                choices: ['Yes: engineer', 'Yes: intern', 'No, the roster is complete'],
-
+                name: 'role',
+                message: 'Please select the team member\'s role:',
+                choices: ['Manager', 'Engineer', 'Intern']
             },
-
         ])
 
 
+        // function to loop through unique keys
+        .then(function ({ name, id, email, role }) {
+            const roleType = "";
+            if (role === 'Engineer') {
+                roleType = 'github';
+            } else if (role === 'Intern') {
+                roleType = 'school';
+            } else {
+                roleType = 'office';
+            }
+            inquirer
+                .promt([
+                    {
+                        type: 'input',
+                        name: 'roleType',
+                        message: 'Please enter the team member\'s ${roleType}:',
+                    },
+                ])
+        })
 
-        // function to initialize app
-        .then((data) => fs.writeFileSync('index.html', generateHTML(data))) //how do you export an HTML?
-        .then(() => console.log('Successfully created an HTML file.'))
-        .catch((err) => console.error(err));
+    // .then((data) => fs.writeFileSync('index.html', generateHTML(data))) //how do you export an HTML?
+    // .then(() => console.log('Successfully created an HTML file.'))
+    // .catch((err) => console.error(err));
 
 
 }
